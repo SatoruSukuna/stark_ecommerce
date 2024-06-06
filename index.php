3f86837cd384d6838396701b5779e28cca37fd82
StarK <105397984+theneonstark@users.noreply.github.com>
2024-06-06 11:23:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.scss">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <img src="images/logo.png" alt="">
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Shop</a>
+            <a href="#">Men</a>
+            <a href="#">Women</a>
+            <a href="#">Blog</a>
+        </nav>
+        <div class="icon">
+            <i class='bx bx-search'></i>
+            <i class='bx bx-user-circle' ></i>
+            <i class='bx bx-heart' ></i>
+            <i class='bx bx-cart-alt' ></i>
+        </div>
+    </header>
+    <section id="main">
+        <div class="main_container">
+            <img src="images/mens-fashion.jpg" alt="">
+            <div class="main_content">
+                <h3>New Collection</h3>
+                <h1>Hottest</h1>
+                <h1>Picks</h1>
+                <h6>Streetwear, Sneakers and More...</h6>
+
+                <button>Learn More</button>
+            </div>
+        </div>
+        <div class="wearable_container">
+        </div>
+    </section>
+    <section>
+
+    </section>
+</body>
+</html>
